dc84aac80ee23d4d08be96da02a484311bb7029b
Mahopa <mahopa@gmail.com>
2014-06-03 13:25:54 -0500

    Added Packages, Constructor Function

---
+++ MonstersAjax/Scripts/monsters.js
@@ -0,0 +1,20 @@
+﻿var MyApp = {};
+//array to hold monsters
+MyApp.Monsters = [];
+//Monster Constructor Function
+MyApp.Monster = function (name, description, picture) {
+    this.name = name;
+    this.description = description;
+    this.picture = picture;
+};
+//Writes table of monsters out to page
+MyApp.WriteTable = function () { };
+//CRUD
+//Create Monster
+MyApp.CreateMonster = function () { };
+//Show info for update
+MyApp.ShowUpdateMonster = function () { };
+//Save info from update
+MyApp.SaveUpdateMonster = function () { };
+//Delete Monster
+MyApp.DeleteMonster = function () { };
